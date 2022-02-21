142538287ec4d700e9978bcf0443cd05321e43ca
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2022-02-21 01:50:51 -0600

    working on action routers

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,46 @@
-// Write your "actions" router here!
+const Action = require('./actions-model')
+const express = require('express')
+const res = require('express/lib/response')
+const router = express.Router()
+
+//endpoints
+
+router.get('/', (req, res) => {
+    Action.get()
+        .then(action => {
+        res.status(200).json(action)
+        }).catch(() => {
+        res.status(500).json('could not retrieve actions')
+    })
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    Action.get(id)
+        .then(action => {
+            if (!action) {
+                res.status(404).json(`no action with id ${id}`)
+            } else {
+                res.status(200).json(action)
+            }
+        }).catch(() => {
+        res.status(500).json('problem retrieving action')
+    })
+})
+
+router.post('/', (req, res) => {
+    const newPost = req.body
+
+    if (!newPost) {
+        res.status(400).json('body required')
+    } else { 
+        Action.post(newPost)
+            .then(post => {
+                res.status(201).json(post)
+            }).catch(() => {
+            res.status(500).json('there was a problem posting your action')
+        })
+    }
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const server = express();
 const projectRouter = require('../api/projects/projects-router')
+const actionRouter = require('../api/actions/actions-router')
 
-
-server.use('/api/projects',projectRouter)
+server.use('/api/projects', projectRouter)
+server.use('/api/actions', actionRouter)
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
